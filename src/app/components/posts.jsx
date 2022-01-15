db9972ce8820e9c375faa2c5f1a7c5f7f4af22c2
slavaOlshanechky <ekaterina.olshanetskaya@gmail.com>
2022-01-15 23:09:46 +0200

    add react-router-dom hooks useHistory useParams

--- src/app/components/posts.jsx
+++ src/app/components/posts.jsx
@@ -1,10 +1,10 @@
 import PostsList from "./postsList";
 import Post from "./post";
-// import query from 'query-string'
-// import _ from "lodash";
+import {useParams} from "react-router-dom";
 
 // const Posts = ({match, location}) => {
-const Posts = ({match,history}) => {
+const Posts = ({}) => {
+    const params=useParams()
     const posts = [
         {id: 1, label: 'post1'},
         {id: 2, label: 'post2'},
@@ -12,7 +12,8 @@ const Posts = ({match,history}) => {
     ]
 
     // const search = query.parse(location.search)
-    const postId = match.params.postId
+    // const postId = match.params.postId
+    const {postId} = params
     // const display = match.params.display
 
     // const cropPosts = search ? _(posts).slice(0).take(search.count).value() : posts
@@ -20,7 +21,7 @@ const Posts = ({match,history}) => {
         {postId ? (
             <>
                 {/*{display && <h2>{display}</h2>}*/}
-                <Post posts={posts} id={postId} history={history}/>
+                <Post posts={posts} id={postId}/>
             </>
         ) : (
             // <PostsList posts={cropPosts}/>
